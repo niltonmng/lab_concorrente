788b6ecc45b7696797b1310438e21848dc47122a
niltonmng <nilton.ginani@ccc.ufcg.edu.br>
2017-08-25 15:45:45 -0300

    2

--- LabConcorrente/src/questao2/Main.java
+++ LabConcorrente/src/questao2/Main.java
@@ -2,13 +2,12 @@ package questao2;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Ponte p = new Ponte();
-		
+	public	static	void main(String[] args)	{
+		Ponte p	= new Ponte();
+		Thread t = new Thread(p);
+		t.start();
 		p.carro("A");
 		p.carro("B");
 		p.carro("A");
-
 	}
 }
--- LabConcorrente/src/questao2/Ponte.java
+++ LabConcorrente/src/questao2/Ponte.java
@@ -1,13 +1,38 @@
 package questao2;
 
-public class Ponte extends Thread {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Ponte implements Runnable {
+
+	private List<String> fila;
+	
 	public Ponte() {
-		super();
+		this.fila = new ArrayList<>();
 	}
 	
-	public void carro(String string) {
-		if (string.equals("A"))	System.out.println("Passei carro A --> B");
-		if (string.equals("B"))	System.out.println("Passei carro B --> A");
+	
+	public void carro(String carro) {
+		fila.add(carro);
 	}
+
+	@Override
+	public void run() {
+		while (!this.fila.isEmpty()) {
+			if (this.fila.contains("A")) {
+				System.out.println("Passei carro A --> B");
+				this.fila.remove("A");
+			} else if (this.fila.contains("B")) {
+				System.out.println("Passei carro B --> A");
+				this.fila.remove("B");
+			}
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		System.out.println("Sem carros");
+	}
+
 }
